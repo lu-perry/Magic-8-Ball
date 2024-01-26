7265e5a6354678e72fe89cf56b9174d0a5936cb5
lu-perry <luperry513@gmail.com>
2024-01-26 12:14:28 +0000

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+// API
+const API_ENDPOINT = 'https://yesno.wtf/api';
+
+/**
+ * STEPS:
+ *
+ * 1. Create a fetchAnswer function and call the API
+ * 2. Output the API's response
+ * 3. Attach fetchAnswer to an event listener
+ * 4. Clear output after 3 seconds
+ * 5. Optional: add loading/error states
+ *
+ */
+async function fetchAnswer() {
+    const response = await fetch(API_ENDPOINT);
+    const jResponse = await response.json();
+    const yesNo = jResponse.answer;
+    const node = document.createTextNode(yesNo);
+    let ans = document.getElementById('answer');
+    ans.appendChild(node);
+}
+
+function clearAnswer() {
+    let ans = document.getElementById('answer');
+    while (ans.firstChild) {
+        ans.removeChild(ans.firstChild);
+    }
+}
+
+let answerButton = document.getElementById('button');
+console.log(answerButton);
+
+function handleClick() {
+    fetchAnswer();
+    setTimeout(clearAnswer, 3000);
+}
+
+answerButton.addEventListener('click', handleClick);
